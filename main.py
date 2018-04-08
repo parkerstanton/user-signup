9df406a80d12654dbf985c7773091064d3766014
Parker Stanton <parkerstanton@gmail.com>
2018-04-07 21:00:07 -0500

    Almost done with user-signup

--- main.py
+++ main.py
@@ -8,12 +8,57 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
+    
     return render_template('signup.html')
 
-@app.route("/")
+@app.route("/", methods = ['POST'])
+def validate():
+    username = request.form['username']
+    password = request.form['password']
+    vpassword = request.form['vpassword']
+    email = request.form['email']
 
-@app.route("/welcome")
-def welcome_page():
-    return render_template('welcome.html')
-def 
+    username_error = ''
+    password_error = ''
+    vpassword_error = ''
+    email_error = ''
+
+    if (not username) or (username.strip() == ""):
+        username_error = "Not a valid username"
+        username = ''
+    else:
+        if len(username) > 20 or len(username) < 3:
+            username_error = "That username is too short/ long"
+            username = ''
+    
+    if (not password) or (password.strip() == ""):
+        password_error = "Not a valid password"
+        password = ''
+    else:
+        if len(password) > 20 or len(password) <3:
+            password_error = "That password is too long/short"
+            password = ''
+    
+    if vpassword != password:
+            vpassword_error = "Oops, your passwords don't match."
+            vpassword = ''
+    if email != "":
+        if '@'not in email:
+            email_error = "Oops, you're missing an '@' sign." 
+            email = ''
+        if '.' not in email:
+            email_error = "Oops, you're missing a period!"
+            email = ''
+        else:
+            if len(email) > 20 or len(email) <3:
+                email_error = "That email is too long/short"
+                email = ''
+    
+    if not username_error and not password_error and not vpassword_error and not email_error:
+        return render_template('welcome.html', username = username)
+    else:   
+        return render_template('signup.html', username_error = username_error, password_error = password_error, vpassword_error = vpassword_error, email_error = email_error, username = username, password = password, vpassword = vpassword, email = email)
+
+
+
 app.run()
